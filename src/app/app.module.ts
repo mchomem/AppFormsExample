fdd5aadf587d67c7142f32c2807fa96c27f85a35
Misael Homem <misael.homem@gmail.com>
2020-07-05 04:21:59 -0300

    Added ngx-toastr to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -18,7 +21,9 @@ import { UserListComponent } from './user/user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({positionClass: 'toast-top-right'})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user',
@@ -7,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   
   save() {
     console.log('Saving...')
+    this.toastr.success('Concluído', 'Sistema')
   }
 
 }
